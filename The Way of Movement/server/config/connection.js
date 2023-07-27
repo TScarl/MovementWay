f843221f847f703d8a73fc3f93cca9788206bf9b
TScar <tscarlett@live.com.au>
2023-07-27 14:31:41 +1000

    she's deployed, mongoDb atlast is setup, might have seeded, dunno, graphQl is 404, yay!

--- The Way of Movement/server/config/connection.js
+++ The Way of Movement/server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://tscarlett:abcd1234@cluster0.q7plj7h.mongodb.net/MovementWay', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/MovementWay', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
